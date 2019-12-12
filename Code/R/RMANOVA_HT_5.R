1e8987c27325ab85c09dd25906296165c08bf7d8
Martina <46865458+martinahb@users.noreply.github.com>
2019-12-12 10:27:37 +0100

    Create RMANOVA_HT_5.R
    Adding RMANOVA for baseline and original serotonin levels

---
+++ Code/R/RMANOVA_HT_5.R
@@ -0,0 +1,229 @@
+library(tidyverse)
+library(ez)
+library(afex)
+
+
+# General parameters for making good looking plots 
+my_theme <- theme_bw() + theme(plot.title = element_text(hjust = 0.5, size = 14, face="bold"), 
+                               plot.subtitle = element_text(hjust = 0.5)) +
+  theme(axis.title=element_text(size=13)) 
+
+# Colour-blind friendly palette
+cbPalette <- c(
+  "#999999", "#E69F00", "#56B4E9", "#009E73",
+  "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+# This file contains the analysis by repeated measure ANOVA (and mixed modelling) where each the time points 
+# aren't limpued. Note that the data-set contains the baseline and none baseline scaled data. (Variables on the 
+# form b_NOA refers to baseline scaled NOA)
+path_data <- "../../Intermediate/Data_tidy.csv"
+data_tidy <- read_csv(path_data, col_types = cols(
+  id = col_factor(), 
+  position = col_factor(), 
+  sample = col_integer(), 
+  Type = col_factor(), 
+  time = col_double(), 
+  NOA = col_double(), 
+  DA = col_double(), 
+  MT_3 = col_double(), 
+  NM = col_double(), 
+  HT_5 = col_double(), 
+  DOPAC = col_double(), 
+  HIAA = col_double(), 
+  HVA = col_double(), 
+  unique_id = col_factor(), 
+  dose = col_factor(), 
+  baseline_NOA = col_double(), 
+  baseline_DA = col_double(), 
+  baseline_HT_5 = col_double(), 
+  b_NOA = col_double(), 
+  b_DA = col_double(), 
+  b_HT_5 = col_double()))
+
+# ================================================================================================
+# Filtering data for missing HT_5-values 
+# ================================================================================================
+specie="HT_5"
+# Calculate number of missing values 
+data_missing <- data_tidy %>%
+  select(position, time, unique_id, specie) %>%
+  filter(time > 0) %>%
+  filter(time != 200) 
+# For the tidyverse (and ggplot) to work
+names(data_missing)[4] <- "specie"
+data_missing <- data_missing %>%
+  group_by(unique_id) %>%
+  summarise(number_na = sum(is.na(specie)))
+
+# The id if no observations are missing 
+data_no_missing <- data_missing %>%
+  filter(number_na == 0) %>%
+  select(unique_id)
+
+# Aggreate number of samples with no missing 
+data_without_missing <- data_tidy %>%
+  filter(time > 0 & time != 200) %>%
+  filter(unique_id %in% data_no_missing$unique_id) %>%
+  mutate(time = as.factor(time)) %>%
+  group_by(dose, position) %>%
+  summarise(count = length(unique(unique_id)))
+
+# ================================================================================================
+# Start of functions
+# ================================================================================================
+  
+# Function performing and plotting results from RMANOVA for Serotonin at different dosages and 
+# positions for non-transformed data. 
+# Args:
+#   data_tidy, the data set in tidy format
+#   test_position, the brain region to be tested
+#   test_dose, the relevant dosage to be compared to control (NaCl)
+# Returns:
+#   ANOVA table and plot 
+RMANOVA_nontransformed_HT_5 <- function(data_tidy, test_position, test_dose){
+  data_tidy_RMANOVA_nontransformed<-data_tidy %>%
+    select(position, unique_id, time, HT_5, Type, dose) %>%
+    mutate(time_cat = as.factor(time)) %>%
+    filter(position == test_position) %>%
+    filter(dose == 'NaCl' | dose == test_dose) %>%
+    filter(time_cat == '20' | time_cat == '40'| time_cat == '60'| time_cat == '80' | time_cat == '100' | time_cat == '120' | time_cat == '140' | time_cat == '160' | time_cat == '180') %>%
+    filter(unique_id %in% data_no_missing$unique_id)
+  
+  result_HT_5 <- ez.glm(
+    id="unique_id"
+    , dv="HT_5"
+    , data=data_tidy_RMANOVA_nontransformed
+    , between = "Type"
+    , within =  "time_cat"
+    , type = 3
+    , return = "univariate"
+    , print.formula = TRUE
+  )
+  
+  lm <- ez.glm(
+    id="unique_id"
+    , dv="HT_5"
+    , data=data_tidy_RMANOVA_nontransformed
+    , between = "Type"
+    , within =  "time_cat"
+    , type = 3
+    , return ="lm"
+    , print.formula = TRUE
+  )
+  
+  nind=length(unique(data_tidy_RMANOVA_nontransformed$unique_id))
+  res <- tibble(res = as.numeric(lm$residuals)) %>%
+    mutate(time = rep(seq(from = 20, by = 20, to = 180), each = nind))
+  
+  title <- str_c("5-HT ", test_position, " ", test_dose)
+  p1 <- ggplot(res, aes(time, res)) + 
+    geom_point() + 
+    geom_hline(yintercept = 0) + 
+    geom_smooth(method = "loess", se = F, color = cbPalette[4], size = 1.2) +
+    labs(title = title , x = "time [min]", y = "residuals") +
+    my_theme
+  
+  title <- str_c("5-HT ", test_position, " ", test_dose)
+  p2 <- ggplot(res, aes(sample = res)) + 
+    geom_qq() + 
+    geom_qq_line() + 
+    labs(title = title) +
+    my_theme 
+  
+  return(list(result_HT_5, p1, p2))
+}
+  
+
+
+# Function performing and plotting results from RMANOVA for Serotonin at different dosages and 
+# positions for transformed (baseline) data. 
+# Args:
+#   data_tidy, the data set in tidy format
+#   test_position, the brain region to be tested
+#   test_dose, the relevant dosage to be compared to control (NaCl)
+# Returns:
+#   ANOVA table and plot 
+RMANOVA_transformed_HT_5 <- function(data_tidy, test_position, test_dose){
+  data_tidy_RMANOVA_transformed<-data_tidy %>%
+    select(id, position, unique_id, time, b_HT_5, Type, dose) %>%
+    mutate(time_cat = as.factor(time)) %>%
+    filter(position == test_position) %>%
+    filter(dose == 'NaCl' | dose == test_dose) %>%
+    filter(time_cat == '20' | time_cat == '40'| time_cat == '60'| time_cat == '80' | time_cat == '100' | time_cat == '120' | time_cat == '140' | time_cat == '160' | time_cat == '180') %>%
+    filter(unique_id %in% data_no_missing$unique_id)
+  
+  result_b_HT_5 <- ez.glm(
+    id="unique_id"
+    , dv="b_HT_5"
+    , data=data_tidy_RMANOVA_transformed
+    , between = "Type"
+    , within =  "time_cat"
+    , type = 3
+    , return = "univariate"
+    , print.formula = TRUE
+  )
+  
+  lm <- ez.glm(
+    id="unique_id"
+    , dv="b_HT_5"
+    , data=data_tidy_RMANOVA_transformed
+    , between = "Type"
+    , within =  "time_cat"
+    , type = 3
+    , return ="lm"
+    , print.formula = TRUE
+  )
+  
+  nind=length(unique(data_tidy_RMANOVA_transformed$unique_id))
+  res <- tibble(res = as.numeric(lm$residuals)) %>%
+    mutate(time = rep(seq(from = 20, by = 20, to = 180), each = nind))
+  
+  title <- str_c("Baseline 5-HT ", test_position, " ", test_dose)
+  p1 <- ggplot(res, aes(time, res)) + 
+    geom_point() + 
+    geom_hline(yintercept = 0) + 
+    geom_smooth(method = "loess", se = F, color = cbPalette[4], size = 1.2) +
+    labs(title = title, x = "time [min]", y = "residuals") +
+    my_theme
+  
+  title <- str_c("Baseline 5-HT ", test_position, " ", test_dose)
+  p2 <- ggplot(res, aes(sample = res)) + 
+    geom_qq() + 
+    geom_qq_line() + 
+    labs(title = title) +
+    my_theme 
+  
+  return(list(result_b_HT_5, p1, p2))
+}
+
+# ================================================================================================
+# Results nontransformed Serotonin (HT_5)
+# ================================================================================================
+
+### Striatum
+RMANOVA_nontransformed_HT_5(data_tidy, test_position="Striatum", test_dose="CC_150.0_mumol/kg") # Type and time significant, violated sphericity, significant corrections
+RMANOVA_nontransformed_HT_5(data_tidy, test_position="Striatum", test_dose="CC_50.0_mumol/kg")  # not significant
+RMANOVA_nontransformed_HT_5(data_tidy, test_position="Striatum", test_dose="CC_16.7_mumol/kg")  # time significant, violated sphericity, significant corrections
+RMANOVA_nontransformed_HT_5(data_tidy, test_position="Striatum", test_dose="CC_5.6_mumol/kg")   # not significant
+
+### Cortex
+RMANOVA_nontransformed_HT_5(data_tidy, test_position="Cortex", test_dose="CC_150.0_mumol/kg")   # Type significant
+RMANOVA_nontransformed_HT_5(data_tidy, test_position="Cortex", test_dose="CC_50.0_mumol/kg")    # Type and time significant, violated sphericity. Significant corrections  
+RMANOVA_nontransformed_HT_5(data_tidy, test_position="Cortex", test_dose="CC_16.7_mumol/kg")    # Type and time significant, violated sphericity. Cannot correct due to too few observations
+RMANOVA_nontransformed_HT_5(data_tidy, test_position="Cortex", test_dose="CC_5.6_mumol/kg")     # Type significant
+
+# ================================================================================================
+# Results transformed Serotonin (b_HT_5)
+# ================================================================================================
+
+### Striatum
+RMANOVA_transformed_HT_5(data_tidy, test_position="Striatum", test_dose="CC_150.0_mumol/kg") # Type significant
+RMANOVA_transformed_HT_5(data_tidy, test_position="Striatum", test_dose="CC_50.0_mumol/kg")  # Type significant
+RMANOVA_transformed_HT_5(data_tidy, test_position="Striatum", test_dose="CC_16.7_mumol/kg")  # not significant 
+RMANOVA_transformed_HT_5(data_tidy, test_position="Striatum", test_dose="CC_5.6_mumol/kg")   # not significant
+
+### Cortex
+RMANOVA_transformed_HT_5(data_tidy, test_position="Cortex", test_dose="CC_150.0_mumol/kg")   # Type significant
+RMANOVA_transformed_HT_5(data_tidy, test_position="Cortex", test_dose="CC_50.0_mumol/kg")    # Type and time significant, violated sphericity. Significant corrections
+RMANOVA_transformed_HT_5(data_tidy, test_position="Cortex", test_dose="CC_16.7_mumol/kg")    # time significant, violated sphericity. cannot correct due to too few observations
+RMANOVA_transformed_HT_5(data_tidy, test_position="Cortex", test_dose="CC_5.6_mumol/kg")     # not significant
